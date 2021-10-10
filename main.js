d4bfd34299ec22afbafa3a01ed7262893dac89c7
ManudeQuevedo <manudequevedo@gmail.com>
2021-10-09 23:31:16 -0500

    Working on the player logic of the game

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 'use strict';
-// Player scores:
-const scorePlayerOne = document.querySelector('#score--0');
-const scorePlayerTwo = document.querySelector('#score--1');
-// Functionality:
-const rollDice = document.querySelector('.btn--roll');
+
+// Players:
+const scorePlayerOne = document.querySelector('#score-1');
+const scorePlayerTwo = document.querySelector('#score-2');
+const currentScorePlayerOne = document.querySelector('#current-1');
+const currentScorePlayerTwo = document.querySelector('#current-2');
+
+// Elements:
+const diceElement = document.querySelector('.dice');
+
+// Functionality
+const rollDice = document.querySelector('.btn-roll');
+const newGame = document.querySelector('.btn-new');
+const holdGame = document.querySelector('.btn-hold');
 
 // Setting up initial scores:
 scorePlayerOne.textContent = 0;
 scorePlayerTwo.textContent = 0;
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 1;
 // Dice:
-const dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
+diceElement.classList.add('hidden');
+
+rollDice.addEventListener('click', () => {
+	// Roll the dice:
+	const dice = Math.trunc(Math.random() * 6) + 1;
+	console.log(dice);
+
+	// Display the dice
+	diceElement.classList.remove('hidden');
+	diceElement.src = `./imgs/dice-${dice}.png`;
+
+	// Checked rolled dice:
+	if (dice !== 1) {
+		currentScore += dice;
+		currentScorePlayerOne.textContent = currentScore;
+	} else {
+	}
+});
